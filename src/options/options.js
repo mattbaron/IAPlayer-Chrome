a881930d30f3e7f939f0c519b98a13090a53b806
Matt Baron <mattbaron@unitf.net>
2016-11-30 00:06:17 -0600

    .

--- src/options/options.js
+++ src/options/options.js
@@ -1,13 +1,58 @@
 // options.js
 
+function Options {
+   var context;
+}
+
+function editStationDialog(id) {
+   var station = context.stationStore.getStation(id);
+   console.log(id);
+
+   $("#stationDialogTitle").text("Edit Station");
+   $("#stationDialog").attr("data-id", station.id);
+   $("#stationDialog #stationName").val(station.name);
+   $("#stationDialog #stationURL").val(station.url);
+   $("#stationDialog").modal("show");
+}
+
+function newStationDialog() {
+   $("#stationDialogTitle").text("Add Station");
+   $("#stationDialog").removeAttr("data-id");
+   $("#stationDialog").modal("show");
+}
+
+function loadStations(context) {
+  var ids = context.stationStore.getIDs();
+   
+   for(var i = 0; i < ids.length; i++) {
+      var station = context.stationStore.getStation(ids[i]);
+      var tr = $("<tr>");
+      $("<td>").html(station.name).appendTo(tr);
+      $("<td>").html(station.url).appendTo(tr);
+      
+      tr.attr("id", station.id);
+      tr.appendTo("#stationTable > tbody");
+   }
+
+   $("#stationTable tr").dblclick(function() {
+      editStationDialog($(this).attr("id"));
+   });
+
+}
+
 $(document).ready(function() {
-
-
 
    $(".nav a").click(function(event) {
       event.preventDefault();
    });
 
+   getContext(function(ctx) {
+      context = ctx;
+      loadStations(context);   
+   });
 
+   $("#newStationButton").click(function() {
+      newStationDialog();
+   });
 
 });
---
+++ src/popup-full/popup.js
@@ -0,0 +1,83 @@
+function Popup() {
+   Popup.context;
+}
+
+function loadStationList(context) {
+   var ids = context.stationStore.getIDs();
+
+   var option = $("<option>");
+   option.attr("value", "");
+   option.text("Select a station...");
+   option.appendTo($("#stationList"));
+
+   for(var i = 0; i < ids.length; i++) {
+      var station = context.stationStore.getStation(ids[i]);
+      var option = $("<option>");
+      option.attr("value", ids[i]);
+      option.text(station.name);
+      option.appendTo($("#stationList"));
+   }
+
+   var currentStation = context.getCurrentStation();
+
+   if(currentStation !== null) {
+      $("#stationList").val(currentStation.id);
+   }
+}
+
+function initEvents(context) {
+
+   context.player.addEventListener("playing", function() {
+      $("#playPauseButton > span").removeClass("glyphicon-play glyphicon-pause");
+      $("#playPauseButton > span").addClass("glyphicon-pause");
+   });
+
+   context.player.addEventListener("pause", function() {
+      $("#playPauseButton > span").removeClass("glyphicon-play glyphicon-pause");
+      $("#playPauseButton > span").addClass("glyphicon-play");
+   });
+}
+
+$(document).ready(function() {
+
+   getContext(function(context) {
+
+      Popup.context = context;
+      initEvents(context);
+      loadStationList(context);
+
+      if(!context.player.paused) {
+         $("#playPauseButton > span").removeClass("glyphicon-play glyphicon-pause");
+         $("#playPauseButton > span").addClass("glyphicon-pause");
+      } else {
+         $("#playPauseButton > span").removeClass("glyphicon-play glyphicon-pause");
+         $("#playPauseButton > span").addClass("glyphicon-play");
+      }
+   });
+
+   $(".btn").click(function(event) {
+      $(this).blur()
+   });
+   
+   $("#stationList").change(function() {
+      Popup.context.playStation($(this).val());
+   });
+   
+   $("#playPauseButton").click(function() {
+      var player = Popup.context.player;
+
+      if(player.src === undefined || player.src.length === 0) {
+         return;
+      }
+
+      if(!player.paused) {
+         Log.i("pause");
+         player.pause();
+      } else {
+         Log.i("play");
+         player.play();
+      }
+   });
+
+});
+
